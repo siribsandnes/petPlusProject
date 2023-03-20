7d0ccfe5128b4f36bbbbe71bac2ccb180bc2b8ba
Kim <lademokim@hotmail.com>
2023-03-20 15:25:23 +0100

    Added FindByID

--- src/main/java/no/ntnu/crudrest/ProductController.java
+++ src/main/java/no/ntnu/crudrest/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.webjars.NotFoundException;
 
 import java.util.*;
 
@@ -32,6 +33,22 @@ public class ProductController {
   public ResponseEntity<Object> getAll() {
     logger.error("Getting all products");
     Iterable<Product> products = productRepository.findAll();
+    return new ResponseEntity<>(products, HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  @Operation(
+          summary = "Get product with ID",
+          description = "Gets one product with the corresponding ID"
+  )
+  Product one(@PathVariable int id) throws NoSuchElementException {
+    return productRepository.findById(id)
+            .orElseThrow(() -> new NoSuchElementException());
+  }
+
+  public ResponseEntity<Object> findAllByProductNameContaining() {
+    logger.error("Getting all products");
+    Iterable<Product> products = productRepository.findAllByProductNameContaining("Katte genser");
     return new ResponseEntity<>(products, HttpStatus.OK);
   }
 
